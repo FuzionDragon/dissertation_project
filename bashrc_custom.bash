b73d95418cbac4c5bc43971354fc44b3f61c96f2
David L <120467347+FuzionDragon@users.noreply.github.com>
2026-01-28 22:50:02 +0000

    Example of checker works, user command checking and ability to run the project in any directory implemented

--- bashrc_custom.bash
+++ bashrc_custom.bash
@@ -6,18 +6,28 @@
 # This is a custom bashrc, use rcfile option of Bash
 [[ $- != *i* ]] && return
 
+# Learning tool logic
+export APP_ACTIVE=1
+export IN_LEVEL=0
+export CURRENT_LEVEL=0
+export NUMBER_OF_USED_COMMANDS=0
+
+# temporary, should be executable path in the future
+export PROJECT_PATH="$(pwd)/Cargo.toml"
+
+# temporary, needs to be location of binary during distribution
+alias check='cargo run --manifest-path $PROJECT_PATH -- -u'
+
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 PS1='[\u@\h \W]\$ '
 PROMPT_COMMAND=''
 
-# Learning tool logic
-export APP_ACTIVE=1
-export CURRENT_LEVEL=0
-export NUMBER_OF_USED_COMMANDS=0
-
 debug_fn () {
-  echo "Command: $BASH_COMMAND with exit status $?";
+  #echo "Command: $BASH_COMMAND with exit status $?";
+  if [[ $IN_LEVEL -eq 1 ]]; then
+    check "$BASH_COMMAND"
+  fi
 }
 
 trap debug_fn debug
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,99 @@
-use serde;
-use serde_json;
-use std::{env, process::Command};
+use clap::Parser;
+use std::{
+    env::{self},
+    process::Command,
+};
 
-const CUSTOM_BASHRC_PATH: &str = "./bashrc_custom.bash";
+//const CUSTOM_BASHRC_PATH: &str = "./bashrc_custom.bash";
+
+// needs persistent storage of the user data: current level, completed level, score, etc, inside a
+// json file, to be modified and fetched on each start and finish of a level.
+// the bash env vars will hold temporary data like: current level (so it can be fetched from the
+// file like a dictionary), number of commands used (possibly not including certain ones like ls
+// and other commands that will be constantly used), other current level details the user will need
+// which can save having to fetch the data from the file again.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// piped user output, to be checked
+    #[arg(short, long)]
+    user_command: Option<String>,
+}
 
 fn main() {
-    match env::var("APP_ACTIVE") {
-        Ok(var) => {
-            println!("Currently in learning environment");
-            println!("{var}");
+    let args = Args::parse();
+
+    match env::current_exe() {
+        Ok(mut exe_path) => {
+            exe_path.pop();
+
+            // temporary, due to rust project file tree
+            // would not need back movements
+            let bashrc_path = format!(
+                "{}/../../bashrc_custom.bash",
+                exe_path.into_os_string().into_string().unwrap()
+            );
+
+            match env::var("APP_ACTIVE") {
+                Ok(_v) => {
+                    if let Some(user_command) = args.user_command {
+                        // function to check output of the command
+                        check_home(&user_command);
+                    } else {
+                        println!("No user output piped");
+                    }
+                }
+                Err(_e) => {
+                    println!("Not currently in learning environment, spawning custom Bash session");
+                    Command::new("bash")
+                        .arg("--rcfile")
+                        .arg(bashrc_path)
+                        .spawn()
+                        .expect("failed to execute process")
+                        .wait()
+                        .expect("failed to wait");
+                }
+            }
         }
-        Err(e) => {
-            println!("{e}");
-            Command::new("bash")
-                .arg("--rcfile")
-                .arg(CUSTOM_BASHRC_PATH)
-                .spawn()
-                .expect("failed to execute process")
-                .wait()
-                .expect("failed to wait");
-        }
+
+        Err(e) => println!("failed to get current exe path: {e}"),
+    };
+}
+
+// different types of checkers: file based, directory based, output based
+// file and directory are self explanatory, simply check if they are in their place and the
+// contents are correct
+// output based will be based on if the user has entered a command and its output matches the one
+// required
+// minimum recommened commands used for the exercise
+//
+// lets have a level that asks the user to show their home directory
+// needs to check if the command entered by the user is successful, before checking its output and
+// comparing it to the checker
+//
+// this function below is used to show how the checker mechanism will work (for output)
+fn check_home(user_command: &str) {
+    let home = env::var("HOME").unwrap();
+    let list = Command::new("ls")
+        .arg(home)
+        .output()
+        .expect("unable to list home directory");
+
+    let user_command = Command::new("bash")
+        .arg("-c")
+        .arg(user_command)
+        .output()
+        .expect("unable to get output from user command");
+
+    let correct_output = String::from_utf8_lossy(&list.stdout);
+    let user_output = String::from_utf8_lossy(&user_command.stdout);
+
+    println!("Home directory has {}", &correct_output);
+    println!("User output is {}", &user_output);
+
+    if correct_output == user_output {
+        println!("Same out put detected, checker found condition has been met");
+    } else {
+        println!("Checker condition not met");
     }
 }
